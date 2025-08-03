645edfdb06457e0f77669ec15e52904beb1cfb3e
Partha <partha@splenta.com>
2025-08-03 19:54:48 +0530

    docs added

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,10 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "docs",
       includes: "../_includes",
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    passthroughFileCopy: true,
   };
 };
